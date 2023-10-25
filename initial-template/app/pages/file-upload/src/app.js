f7bf4b95e08068730f67a5a077778369aa238e77
filipi <filipisee@hotmail.com>
2023-10-25 19:37:53 -0300

    structuring main view.js

--- initial-template/app/pages/file-upload/src/app.js
+++ initial-template/app/pages/file-upload/src/app.js
@@ -1,14 +1,5 @@
 import Clock from './deps/clock.js';
 
-const fileUpload = document.getElementById('fileUpload')
-const btnUploadVideo = document.getElementById('btnUploadVideos')
-const fileSize = document.getElementById('fileSize')
-const fileInfo = document.getElementById('fileInfo')
-const txtfileName = document.getElementById('fileName')
-const fileUploadWrapper = document.getElementById('fileUploadWrapper')
-const elapsed = document.getElementById('elapsed')
-
-
 fileUpload.addEventListener('change', onChange)
 btnUploadVideo.addEventListener('click', () => {
     // trigger file input
---
+++ initial-template/app/pages/file-upload/src/view.js
@@ -0,0 +1,11 @@
+export default class View {
+  #fileUpload = document.getElementById("fileUpload");
+  #btnUploadVideo = document.getElementById("btnUploadVideos");
+  #fileSize = document.getElementById("fileSize");
+  #fileInfo = document.getElementById("fileInfo");
+  #txtfileName = document.getElementById("fileName");
+  #fileUploadWrapper = document.getElementById("fileUploadWrapper");
+  #elapsed = document.getElementById("elapsed");
+
+  #canvas = document.getElementById("preview-144p");
+}
