8ee9df0e29a549bfdf27d7f1751ad2d6c967db75
filipi <filipisee@hotmail.com>
2023-10-25 22:38:00 -0300

    added encodeConfigs

--- initial-template/app/pages/file-upload/src/worker/worker.js
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -1,4 +1,33 @@
+const qvgaContraints = {
+  width: 320,
+  height: 240,
+};
+
+const vgaConstraints = {
+  width: 640,
+  height: 480,
+};
+const hdConstraints = {
+  width: 1280,
+  height: 720,
+};
+
+const encoderConfig = {
+  bitrate: 10e6,
+  // WebM
+  codec: "vp09.00.10.08",
+  pt: 4,
+  hardwareAcceleration: "prefer-software",
+
+  // MP4
+  /* codec: "avc1.42002A",
+  pt: 1,
+  hardwareAcceleration: "prefer-hardware",
+  avc: {format: 'annexb'} */
+};
+
 onmessage = (data) => {
+  debugger;
   setTimeout(() => {
     self.postMessage({
       status: "done",
